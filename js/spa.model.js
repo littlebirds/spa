e7c05d879e37ba4ef7a16eb2610152dc52e9690a
littlebirds <gangzhi2000@gmail.com>
2015-01-29 07:48:23 -0500

    half way in chapter 6

--- js/spa.model.js
+++ js/spa.model.js
@@ -20,12 +20,13 @@ spa.model = (function () {
         anon_user : null,
 		cid_serial: 0,
         people_cid_map : {},
-        people_db : TAFFY()
+        people_db : TAFFY(),
+		is_connected : false
     },
     isFakeData = true,
     
     personProto, makeCid, clearPeopleDb, completeLogin,
-		makePerson, removePerson, people, initModule;
+		makePerson, removePerson, people, chat, initModule;
     
 	
     // The people object API
@@ -189,35 +190,110 @@ spa.model = (function () {
 		};
     } ());
     
-    initModule = function() {
-        var i, people_list, person_map;
-        
+
+    
+	// The chat object API
+	// -------------------
+	// The chat object is available at spa.model.chat.
+	// The chat object provides methods and events to manage
+	// chat messaging. Its public methods include:
+	// * join() - joins the chat room. This routine sets up
+	// the chat protocol with the backend including publishers
+	// for 'spa-listchange' and 'spa-updatechat' global
+	// custom events. If the current user is anonymous,
+	// join() aborts and returns false.
+	// ...
+	//
+	// jQuery global custom events published by the object include:
+	// ...
+	// * spa-listchange - This is published when the list of
+	// online people changes in length (i.e. when a person
+	// joins or leaves a chat) or when their contents change
+	// (i.e. when a person's avatar details change).
+	// A subscriber to this event should get the people_db
+	// from the people model for the updated data.
+	// ...
+	//
+	chat = (function () {
+		var
+		_publish_listchange,
+		_update_list, _leave_chat, join_chat;
+		
+		// Begin internal methods
+		_update_list = function( arg_list ) {
+			var i, person_map, make_person_map,
+			people_list = arg_list[ 0 ];
+			clearPeopleDb();
+
+			PERSON:
+			for ( i = 0; i < people_list.length; i++ ) {
+				person_map = people_list[i];
+				if ( ! person_map.name ) { continue PERSON; }
+				
+				// if user defined, update css_map and skip remainder
+				 if ( stateMap.user && stateMap.user.id === person_map._id ) {
+					 stateMap.user.css_map = person_map.css_map;
+					 continue PERSON;
+				 }
+				make_person_map = {
+					cid : person_map._id,
+					css_map : person_map.css_map,
+					id : person_map._id,
+					name : person_map.name
+				};		
+				makePerson(make_person_map);
+			}
+			stateMap.people_db.sort( 'name' );
+		}; 
+			
+		_publish_listchange = function ( arg_list ) {
+			_update_list( arg_list );
+			$.gevent.publish( 'spa-listchange', [ arg_list ] );
+		};
+		// End internal methods	---------------------------------
+		
+		_leave_chat = function () {
+			var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+			stateMap.is_connected = false;
+			if (sio) { sio.emit( 'leavechat' ); }
+		};
+		
+		join_chat = function () {
+			var sio;
+			if ( stateMap.is_connected ) { return false; }
+			
+			if ( stateMap.user.get_is_anon() ) {
+				console.warn( 'User must be defined before joining chat');
+				return false;
+			}
+			sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+			sio.on( 'listchange', _publish_listchange );
+			stateMap.is_connected = true;
+			return true;
+		};
+		
+		return {
+			_leave : _leave_chat,
+			join : join_chat
+		};
+	}());
+		
+	
+	// Begin public method /initModule/
+	initModule = function() { 
         //initialize anonymous person
         stateMap.anon_user = makePerson({
             cid: configMap.anon_id,
             id: configMap.anon_id,
             name: 'anonymous'
         });
-        stateMap.user = stateMap.anon_user;
-        
-        if(isFakeData) {
-            people_list = spa.fake.getPeopleList();
-            for ( i =0; i < people_list.length; ++i) 
-            {
-                person_map = people_list[i];
-                makePerson({
-                    cid : person_map._id,
-                    css_map: person_map.css_map,
-                    id: person_map._id,
-                    name: person_map.name
-                }); 
-            }
-        }
-    };
-    
+        stateMap.user = stateMap.anon_user; 
+    };// End public method /initModule/
+	
     return {
         initModule: initModule,
-        people: people
+		chat: chat,
+        people: people 
     };
     
 }());
